675d2885239e85fce9386bf6422972392ef4d23e
Palestinow <thiagupapapa@gmail.com>
2026-03-02 20:29:45 -0300

    Add files via upload
    web avançado

---
+++ aula1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+
+        resultado = 0
+
+        Console.WriteLine("insert the number");
+
+        float num = float.Parse(Console.ReadLine());
+        
+        resultado = num . 2;
+
+         Console.WriteLine("the answer is " + resultado);
+
+        /*Console.WriteLine("CALCULADORA");
+
+        Console.WriteLine("insert the first number");
+
+        float firstnum = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("select the operation: /  *   -   + ");
+
+        char operation = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("insert the second number");
+
+        float secondnum = float.Parse(Console.ReadLine());
+
+        float results = 0; 
+        
+            switch(operation)
+            {   
+                case '/':
+                    results = firstnum / secondnum;                
+                    break;
+                case '*':
+                    results = firstnum * secondnum;
+                    break;
+                case '-':
+                    results = firstnum - secondnum;
+                    break;
+                case '+':
+                    results = firstnum + secondnum;     
+                    break;
+                default:
+                Console.WriteLine("Invalid operation.");
+                return;    
+            }
+        Console.WriteLine("the answer is " + results);
+    */}
+}
+
+/*Console.WriteLine("what is your name?");
+
+    string name = Console.ReadLine();
+
+Console.WriteLine("your name is " + name);
+
+Console.WriteLine("how old are you?");
+
+    float age = float.Parse(Console.ReadLine());
+
+    if(age > 17)
+
+        Console.WriteLine("you are of legal age");
+
+    else
+    {
+        Console.WriteLine("well... you still are a children?");
+    }    
+    */
+
+
