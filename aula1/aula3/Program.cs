e21fca6875f19a8518b4bf36496abd4b2e3f6b1f
Palestinow <thiagupapapa@gmail.com>
2026-03-16 21:27:56 -0300

    Add files via upload

---
+++ aula1/aula3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+/*class Curso{
+    public int codigo;
+    public int duracao;
+}   
+
+    public enum Turno
+    {
+        Manha,
+        Tarde,
+        Noite
+    }
+
+class Turma{
+    public int codigo;
+    public string curso;
+    public Turno turno;
+    public string[] estudantes;
+}
+
+/*class Disciplina{
+    public int matricula;
+    public string nome;
+    public int turma;
+}
+
+class MatriculaDisciplina{
+    public string estudante;
+    public string
+}
+
+class Program{
+    static void Main(){ 
+
+        Curso engsoft = new Curso();
+
+        engsoft.codigo = 1;
+        engsoft.duracao = 6;
+
+        Console.WriteLine($"Codigo do curso: {engsoft.codigo}");
+        Console.WriteLine($"Duraçao do curso: {engsoft.duracao}");
+
+        Turma racuja = new Turma();
+        racuja.codigo = 1;
+        racuja.curso = "engsoft";
+        racuja.turno = Turno.Noite;
+        racuja.estudantes = new string[] { "Ana", "Carlos", "João" };
+
+            Console.WriteLine(racuja.codigo);
+            Console.WriteLine(racuja.curso);
+            Console.WriteLine(racuja.turno);
+
+     }
+}*/
+
+
+/*Crie um método que recebe um array de double e retorne o maior número desse array.
+3. Altere o método do ex. 2 para retornar uma tupla com maior e menor número. Exiba
+em uma saída no console.
+*/
+class Program{
+    static void Main(){
+
+        double[] numeros = 
+    }
+
+
+
+}
